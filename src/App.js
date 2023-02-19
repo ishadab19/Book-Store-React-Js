53638f9c133441beb537472e6193a5bef62be933
shadab <me.shadab19@gmail.com>
2023-02-19 15:48:29 +0530

    [Added-Fetching data to Admin dashBoard from backend ]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from "react-router-dom"
+import Header from './components/Header';
+import Home from './components/home/Home';
+import OrderSuccess from './components/order/OrderSuccess';
+import MyOrders from './components/order/MyOrders';
+import AddCart from './components/cart/AddCart';
+import LoginForm from './components/user/LoginForm';
+import RegisterForm from './components/user/RegistrationForm';
+import ForgetPassword from "./components/user/ForgotPassword";
+import ResetPassword from './components/user/ResetPassword';
+import Dashboard from './dashboard/AppDashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/ordersuccess' element={<OrderSuccess />} />
+          <Route path='/myorders' element={<MyOrders />} />
+          <Route exact path='/mycart' element={<AddCart />}></Route>
+          <Route path='/login' element={<LoginForm />} ></Route>
+          <Route path='/home' element={<Home />} />
+          <Route path='registration' element={<RegisterForm />}></Route>
+          <Route path='forgotpass' element={<ForgetPassword />}></Route>
+          <Route path='/resetpass' element={<ResetPassword />}></Route>
+          <Route path='/dash' element={<Dashboard/>}></Route>
+        </Routes>
+
+      </div>);
+  }
 }
 
 export default App;
